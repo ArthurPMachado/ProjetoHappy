007944bd2c7f409d133be783666f032fc22acb37
Arthur Pereira Machado <machadoparthur1@gmail.com>
2020-10-12 20:58:12 -0300

    Feat: start landing page styling

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,9 +1,28 @@
 import React from 'react';
 
+import logoImg from './assets/images/logo.svg';
+
+import './styles/global.css';
+import './styles/pages/landing.css';
+
 function App() {
   return (
-    <div className="App">
-      <h1>Hello World!</h1>
+    <div id="page-landing">
+      <div className="content-wrapper">
+        <img src={logoImg} alt="Happy"/>
+
+        <main>
+          <h1>Leve felicidade para o mundo</h1>
+          <p>Visite orfanatos e mude o dia de muitas crianças.</p>
+        </main>
+
+        <div className="location">
+          <strong>São Paulo</strong>
+          <span>São Paulo</span>
+
+          <a href="" className="enter-app"></a>
+        </div>
+      </div>
     </div>
   );
 }
