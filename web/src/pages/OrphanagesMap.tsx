70e1f1fa35b9fe8b6c5c44f4719eb45d6499dc77
Arthur Pereira Machado <machadoparthur1@gmail.com>
2020-10-12 22:16:13 -0300

    Feat: add style to orphanages map

---
+++ web/src/pages/OrphanagesMap.tsx
@@ -0,0 +1,37 @@
+/* eslint-disable no-use-before-define */
+/* eslint-disable react/jsx-filename-extension */
+import React from 'react';
+import { FiPlus } from 'react-icons/fi';
+import { Link } from 'react-router-dom';
+
+import mapMarkerImg from '../assets/images/map-marker.svg';
+
+import '../styles/pages/orphanages-map.css';
+
+function OrphanagesMap() {
+  return (
+    <div id="page-map">
+      <aside>
+        <header>
+          <img src={mapMarkerImg} alt="Happy" />
+
+          <h2>Escolha um orfanato no mapa</h2>
+          <p>Muitas crianças estãoesperando a sua visita :)</p>
+        </header>
+
+        <footer>
+          <strong>São Paulo</strong>
+          <span>São Paulo</span>
+        </footer>
+      </aside>
+
+      <div />
+
+      <Link to="" className="create-orphanage">
+        <FiPlus size={32} color="#FFF" />
+      </Link>
+    </div>
+  );
+}
+
+export default OrphanagesMap;
---
+++ web/src/routes.tsx
@@ -0,0 +1,20 @@
+/* eslint-disable no-use-before-define */
+/* eslint-disable react/jsx-filename-extension */
+import React from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+
+import Landing from './pages/Landing';
+import OrphanagesMap from './pages/OrphanagesMap';
+
+function Routes() {
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route path="/" exact component={Landing} />
+        <Route path="/app" component={OrphanagesMap} />
+      </Switch>
+    </BrowserRouter>
+  );
+}
+
+export default Routes;
