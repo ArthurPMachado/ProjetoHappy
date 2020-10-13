d10dcb69c413e22aa963be3a424d1c03c3ffb82e
Arthur Pereira Machado <machadoparthur1@gmail.com>
2020-10-12 22:41:24 -0300

    Feat: add map with mapbox

--- web/src/pages/OrphanagesMap.tsx
+++ web/src/pages/OrphanagesMap.tsx
@@ -3,11 +3,12 @@
 import React from 'react';
 import { FiPlus } from 'react-icons/fi';
 import { Link } from 'react-router-dom';
-import { Map } from 'react-leaflet';
+import { Map, TileLayer } from 'react-leaflet';
 
 import mapMarkerImg from '../assets/images/map-marker.svg';
 
 import '../styles/pages/orphanages-map.css';
+import 'leaflet/dist/leaflet.css';
 
 function OrphanagesMap() {
   return (
@@ -26,7 +27,14 @@ function OrphanagesMap() {
         </footer>
       </aside>
 
-      <div />
+      <Map
+        center={[-23.5338423, -46.5656102]}
+        zoom={15}
+        style={{ width: '100%', height: '100%' }}
+      >
+        {/* <TileLayer url="https://a.tile.openstreetmap.org/{z}/{x}/{y}.png" /> */}
+        <TileLayer url={`http://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/256/{z}/{x}/{y}@2x?access_token=${process.env.REACT_APP_MAPBOX_TOKEN}`} />
+      </Map>
 
       <Link to="" className="create-orphanage">
         <FiPlus size={32} color="#FFF" />
