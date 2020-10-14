f919fa37d39d24ba8fbcc6ed005f2a6d58c2e214
Arthur Pereira Machado <machadoparthur1@gmail.com>
2020-10-14 19:55:43 -0300

    Feat: add link to orphanage detail

--- web/src/pages/OrphanagesMap.tsx
+++ web/src/pages/OrphanagesMap.tsx
@@ -1,14 +1,24 @@
 /* eslint-disable no-use-before-define */
 /* eslint-disable react/jsx-filename-extension */
 import React from 'react';
-import { FiPlus } from 'react-icons/fi';
+import { FiArrowRight, FiPlus } from 'react-icons/fi';
 import { Link } from 'react-router-dom';
-import { Map, TileLayer } from 'react-leaflet';
+import {
+  Map, Marker, Popup, TileLayer,
+} from 'react-leaflet';
+import Leaflet from 'leaflet';
 
 import mapMarkerImg from '../assets/images/map-marker.svg';
 
 import '../styles/pages/orphanages-map.css';
 import 'leaflet/dist/leaflet.css';
+
+const mapIcon = Leaflet.icon({
+  iconUrl: mapMarkerImg,
+  iconSize: [58, 68],
+  iconAnchor: [29, 68],
+  popupAnchor: [170, 2],
+});
 
 function OrphanagesMap() {
   return (
@@ -18,7 +28,7 @@ function OrphanagesMap() {
           <img src={mapMarkerImg} alt="Happy" />
 
           <h2>Escolha um orfanato no mapa</h2>
-          <p>Muitas crianças estãoesperando a sua visita :)</p>
+          <p>Muitas crianças estão esperando a sua visita :)</p>
         </header>
 
         <footer>
@@ -34,6 +44,23 @@ function OrphanagesMap() {
       >
         {/* <TileLayer url="https://a.tile.openstreetmap.org/{z}/{x}/{y}.png" /> */}
         <TileLayer url={`http://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/256/{z}/{x}/{y}@2x?access_token=${process.env.REACT_APP_MAPBOX_TOKEN}`} />
+
+        <Marker
+          icon={mapIcon}
+          position={[-23.5338423, -46.5656102]}
+        >
+          <Popup
+            closeButton={false}
+            minWidth={240}
+            maxWidth={240}
+            className="map-popup"
+          >
+            Lar das meninas
+            <Link to="">
+              <FiArrowRight size={20} color="#FFF" />
+            </Link>
+          </Popup>
+        </Marker>
       </Map>
 
       <Link to="" className="create-orphanage">
