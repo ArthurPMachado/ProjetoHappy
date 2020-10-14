4bb4fae72e7d01d23eb79de0e138911e4f9c2af2
Arthur Pereira Machado <machadoparthur1@gmail.com>
2020-10-14 15:22:13 -0300

    Feat: configura validation error with yup

--- server/src/controllers/OrphanagesController.ts
+++ server/src/controllers/OrphanagesController.ts
@@ -1,8 +1,9 @@
 import { Request, Response } from 'express';
 import { getRepository } from 'typeorm';
+import * as Yup from 'yup';
+import orphanages_view from '../views/orphanages_view';
 
 import Orphanage from '../models/Orphanage';
-import orphanages_view from '../views/orphanages_view';
 
 export default {
   async create(request: Request, response: Response) {
@@ -19,9 +20,10 @@ export default {
     const orphanagesRepository = getRepository(Orphanage);
 
     const requestImages = request.files as Express.Multer.File[];
+
     const images = requestImages.map((image) => ({ path: image.filename }));
 
-    const orphanage = orphanagesRepository.create({
+    const data = {
       name,
       latitude,
       longitude,
@@ -30,7 +32,28 @@ export default {
       opening_hours,
       open_on_weekends,
       images,
+    };
+
+    const schema = Yup.object().shape({
+      name: Yup.string().required(),
+      latitude: Yup.number().required(),
+      longitude: Yup.number().required(),
+      about: Yup.string().required().max(300),
+      instructions: Yup.string().required(),
+      opening_hours: Yup.string().required(),
+      open_on_weekends: Yup.boolean().required(),
+      images: Yup.array(
+        Yup.object().shape({
+          path: Yup.string().required(),
+        }),
+      ),
     });
+
+    await schema.validate(data, {
+      abortEarly: false,
+    });
+
+    const orphanage = orphanagesRepository.create(data);
 
     await orphanagesRepository.save(orphanage);
 
